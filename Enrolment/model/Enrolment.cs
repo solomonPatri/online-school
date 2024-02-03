654d7d76d1b9fe366292283c90b9ceabf7e8072f
Solomon Patricia <solomonpatricia333@gmail.com>
2024-02-03 17:56:29 +0200

    added models

---
+++ Enrolment/model/Enrolment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_school.Enrolment.model
+{
+    internal class Enrolment
+    {
+    }
+}
